2585077b4d081f40a379c0a5ddcbe2b020d4b18d
WiktorJurek <WiktorJurek@users.noreply.github.com>
2023-05-31 17:30:02 +0200

    map from images test

--- resources/js/Map.vue
+++ resources/js/Map.vue
@@ -1,27 +1,33 @@
 <template>
-    <div style="height:600px; width:800px">
-      <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]" :use-global-leaflet="false">
+    <div style="height:900px;">
+      <l-map ref="map" v-model:zoom="zoom" :center="[-62.950,-118.000]" :use-global-leaflet="false" :options="{attributionControl: false}" :min-zoom="2" :max-zoom="4">
         <l-tile-layer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="../assets/images/maps/bayview/{z}/{x}/{y}.png"
           layer-type="base"
-          name="OpenStreetMap"
+          name="bayview"
+          :tms="true"
+          :no-wrap="true"
         ></l-tile-layer>
+
+        <l-marker :lat-lng="coordinates" draggable> </l-marker>
       </l-map>
     </div>
   </template>
   
-  <script>
+  <script lang="ts">
   import "leaflet/dist/leaflet.css";
-  import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-  
+  import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+  import type L from "leaflet";
   export default {
     components: {
       LMap,
       LTileLayer,
+      LMarker,
     },
     data() {
       return {
         zoom: 4,
+        coordinates: [50, 50] as L.LatLngExpression,
       };
     },
   };
