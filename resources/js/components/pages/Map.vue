f5ca2ad26c57e9c8be8a7e7109986fa14ed6b81a
WiktorJurek <WiktorJurek@users.noreply.github.com>
2023-06-04 19:47:55 +0200

    api get - promise to async

--- resources/js/components/pages/Map.vue
+++ resources/js/components/pages/Map.vue
@@ -1,27 +1,33 @@
 <template>
-    <!-- <h1>{{ gameData[0].name }} map {{ mapData[0].name }}</h1> -->
-    <div style="height:900px;">
-      <l-map ref="map" v-model:zoom="zoom" :center="[-62.950,-118.000]" :use-global-leaflet="false" :options="{attributionControl: false}" :min-zoom="2" :max-zoom="4">
-        <l-tile-layer
-          :url="mapName"
-          layer-type="base"
-          name="bayview"
-          :tms="true"
-          :no-wrap="true"
-        ></l-tile-layer>
+    <div v-if="dataLoaded">
+      <div v-for="map in mapData" :key="map.id">
+        {{ map.coords }}
+      </div>
+      <h1>{{ gameData[0].name }} map {{ mapData[0].name }}</h1>
+      
+      <div style="height:900px;">
+        <l-map ref="map" v-model:zoom="zoom" :center="[-62.95,-118]" :use-global-leaflet="false" :options="{attributionControl: false}" :min-zoom="2" :max-zoom="4">
+          <l-tile-layer
+            :url="mapName"
+            layer-type="base"
+            name="bayview"
+            :tms="true"
+            :no-wrap="true"
+          ></l-tile-layer>
 
-        <l-layer-group v-for="(markerGroup, groupName) in markersData" :key="groupName">
-          <l-marker v-for="marker in markerGroup" :key="marker.id" :lat-lng="marker.coords">
-            <l-icon :icon-size="[30,30]" :icon-url="`/assets/icons/${mapData[0].name}/${groupName}.png`"></l-icon>
-          </l-marker>
-        </l-layer-group>
+          <l-layer-group v-for="(markerGroup, groupName) in markersData" :key="groupName">
+            <l-marker v-for="marker in markerGroup" :key="marker.id" :lat-lng="marker.coords">
+              <l-icon :icon-size="[30,30]" :icon-url="`/assets/icons/${mapData[0].name}/${groupName}.png`"></l-icon>
+            </l-marker>
+          </l-layer-group>
 
-      </l-map>
+        </l-map>
+      </div>
+      <div>
+      <ul>
+        <li v-for="marker in markersData" :key="marker.id">{{ marker.type + marker.coords }}</li>
+      </ul>
     </div>
-    <div>
-    <ul>
-      <li v-for="marker in markersData" :key="marker.id">{{ marker.type + marker.coords }}</li>
-    </ul>
   </div>
 </template>
   
@@ -41,35 +47,40 @@
     data() {
       return {
         zoom: 4,
-        gameData: {},
-        mapData: {},
-        mapName: '',
-        markersData: {},
+        gameData: null,
+        mapData: null,
+        mapName: null,
+        markersData: null,
+        dataLoaded: false
       };
     },
     mounted() {
 
       const gameName = this.$route.params.gameName;
+      this.fetchData(gameName);
 
-      axios.get(`/api/v1/games/getbyname/${gameName}`)
-        .then(response => {
-          this.gameData = response.data;
-          return axios.get(`/api/v1/maps/getbygame/${response.data[0].id}`);
-        })
-        .then(response => {
-          this.mapName = `../assets/images/maps/${response.data[0].name}/{z}/{x}/{y}.png`;
-          this.mapData = response.data;
-
-          return axios.get(`/api/v1/markers/getbygame/${response.data[0].game}`);
-        })
-        .then(response => {
-          this.markersData = response.data;
-        })
-        .catch(error => {
-          console.error(error);
-        });
     },
     methods: {
+
+      async fetchData(gameName) {
+
+        try {
+          const getGame = await axios.get(`/api/v1/games/getbyname/${gameName}`);
+          this.gameData = getGame.data;
+
+          const getMap = await axios.get(`/api/v1/maps/getbygame/${getGame.data[0].id}`);
+          this.mapName = `../assets/images/maps/${getMap.data[0].name}/{z}/{x}/{y}.png`;
+          this.mapData = getMap.data;
+
+          const getMarkers = await axios.get(`/api/v1/markers/getbygame/${getGame.data[0].game}`);
+          this.markersData = getMarkers.data;
+
+          // console.log(this.mapData);
+          this.dataLoaded = true;
+        } catch (error) {
+          console.error(error);
+        }
+      },    
     },
   };
   </script>
