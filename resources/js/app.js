2fbee7bb98971603d5ee76d4e411b57ff4a33b84
WiktorJurek <WiktorJurek@users.noreply.github.com>
2023-06-10 15:15:45 +0200

    Laravel Breeze authentication + pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './axios.js';
 import App from './components/App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
