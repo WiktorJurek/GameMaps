703e800bb05b8112e43bc7fea8552671a211eadc
WiktorJurek <WiktorJurek@users.noreply.github.com>
2023-06-11 20:28:01 +0200

    underground 2 color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#353533",
+          "secondary": "#d1dcaf",
+          "accent": "#c7f38f",
+          "neutral": "#a6a8a5",
+          "base-100": "#040903",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
