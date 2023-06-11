7b1a557c1398a04d8c488fc4f4701a461a8aa906
WiktorJurek <WiktorJurek@users.noreply.github.com>
2023-06-11 20:29:34 +0200

    user icon

--- resources/js/components/common/Header.vue
+++ resources/js/components/common/Header.vue
@@ -7,8 +7,8 @@
             <div class="flex-none">
                 <div class="dropdown dropdown-end">
                 <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                    <img src="" />
+                    <div class="w-10 rounded-full bg-neutral">
+                    <img src="/assets/icons/user_white.png" />
                     </div>
                 </label>
                 <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
